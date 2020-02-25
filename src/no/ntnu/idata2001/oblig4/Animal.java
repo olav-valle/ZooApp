3c37c6a30481012dea99bdd8dff5802e86f71b1d
Olav <olavval@stud.ntnu.no>
2020-02-25 12:05:25 +0100

    adds classes from attachments

---
+++ src/no/ntnu/idata2001/oblig4/Animal.java
@@ -0,0 +1,17 @@
+package no.ntnu.idata2001.oblig4;
+
+public abstract class Animal {
+  public final String name;
+  public final int animalID;
+
+  public Animal(String name, int code) {
+    this.name = name;
+    this.animalID = code;
+  }
+
+  @Override
+  public String toString() {
+    return "Animal [name=" + name + ", code=" + animalID + "]";
+  }
+}
+
---
+++ src/no/ntnu/idata2001/oblig4/Mammal.java
@@ -0,0 +1,8 @@
+package no.ntnu.idata2001.oblig4;
+
+public class Mammal extends Animal {
+  public Mammal(String name, int code) {
+    super(name, code);
+  }
+}
+
